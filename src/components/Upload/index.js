26a4d97346a83db9572becc7ae32dc3be2dc16a3
Daniel Rezende <daniel.zende@gmail.com>
2020-05-11 17:01:44 -0300

    teste

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -6,7 +6,7 @@ import { DropContainer, UploadMessage } from "./styles";
 export default class Upload extends Component {
   renderDragMessage = (isDragActive, isDragReject) => {
     if (!isDragActive) {
-      return <UploadMessage>Arraste arquivos aqui ...</UploadMessage>;
+      return <UploadMessage>Teste arquivos aqui ...</UploadMessage>;
     }
 
     if (isDragReject) {
